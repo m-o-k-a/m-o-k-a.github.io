193ef13d1fdb0e211ef48f2ba099305a5d188f84
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2019-05-01 10:41:46 +0200

    Update FormToEmail.php

--- ProjectsWeb/mokastudy/Php/FormToEmail.php
+++ ProjectsWeb/mokastudy/Php/FormToEmail.php
@@ -9,6 +9,7 @@
     $emailFrom = "eddy.ikhlef@protonmail.com";
     $emailSubject = "[FROM: MY WEBSITE] $messageObject";
     $emailBody = "<You have received a new message from the user $name ($senderEmail).>\n". "$message".
+    $headers = "message from your personnal website"
 
     //SEND THE MESSAGE
     mail($emailFrom,$emailSubject,$emailBody,$headers);
