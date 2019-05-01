3b99476345e3ad5dfe8effc347b3f7dfe024eee5
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2019-05-01 10:37:12 +0200

    Added v2 website

---
+++ ProjectsWeb/mokastudy/Php/FormToEmail.php
@@ -0,0 +1,15 @@
+<?php
+	//THROW ELEMENT FROM HTML
+  	$name = $_POST['name'];
+  	$senderEmail = $_POST['email'];
+  	$messageObject = $_POST['object'];
+  	$message = $_POST['message'];
+
+  	//COMPOSE MESSAGE
+    $emailFrom = "eddy.ikhlef@protonmail.com";
+    $emailSubject = "[FROM: MY WEBSITE] $messageObject";
+    $emailBody = "<You have received a new message from the user $name ($senderEmail).>\n". "$message".
+
+    //SEND THE MESSAGE
+    mail($emailFrom,$emailSubject,$emailBody,$headers);
+?>
